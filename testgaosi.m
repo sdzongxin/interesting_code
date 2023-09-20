dc2fb17bf56ac29ebda2f0489d50cdfd1189288d
sdzongxin <144077843+sdzongxin@users.noreply.github.com>
2023-09-20 17:02:52 +0800

    Update testgaosi.m

--- testgaosi.m
+++ testgaosi.m
@@ -1,1 +1,9 @@
-% try to predict champion of Paris major 2023
+% 一些平平无奇的画图技巧
+x = linspace(-10,10);
+cc = [0.1, 0.5 , 1, 5, 32];
+for i = 1 :length(cc)
+    y = exp(-x.^2./cc(i));
+    plot(x,y)
+    title(['the ',num2str(i),' times, cc =', num2str(cc(i))])
+    pause(0.5)
+end
